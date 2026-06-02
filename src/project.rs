fd794b7e5ad4a5896650a9ab2dfc1afeea777ee9
pox <iampoxel6@gmail.com>
2026-06-02 21:45:45 +0330

    feat(template): new template system is working but far from complete.

--- src/project.rs
+++ src/project.rs
@@ -1,12 +1,10 @@
 use std::{
     fs::{self, File},
     io,
+    path::{Path, PathBuf},
 };
 
-use crate::{
-    cli::{Cli, prompt},
-    template::Template,
-};
+use crate::cli::{Cli, prompt};
 
 #[derive(Debug, Clone, Default)]
 pub struct Project {
@@ -39,108 +37,60 @@ impl From<Cli> for Project {
 
 impl Project {
     pub fn init(&self) -> Result<(), io::Error> {
-        let structure = get_structure(&self);
-        craete_dirs(&structure)?;
-        create_files(&structure)?;
+        let structure = get_structure();
+        craete_dirs(&structure, &self)?;
+        create_files(&structure, &self)?;
         Ok(())
     }
 }
 
-pub enum ProjectPaths {
-    Root,
-    MainResources,
-    TestResources,
-    Working,
-    TestWorking,
-}
-
-#[derive(Default, Debug)]
-pub struct ProjectFile {
-    pub name: String,
-    pub path: String,
-    pub template: Template,
-    project: Project,
-}
-
-impl ProjectFile {
-    pub fn new(project: &Project) -> Self {
-        Self {
-            project: project.clone(),
-            ..Default::default()
+fn craete_dirs(dirs: &Vec<PathBuf>, project: &Project) -> Result<(), io::Error> {
+    for dir in dirs {
+        let dir = dir.parent().unwrap();
+        if dir.is_dir() {
+            fs::create_dir_all(replace_placeholders(project, dir))?;
         }
-    }
-    pub fn name(mut self, name: &str) -> Self {
-        self.name = name.to_string();
-        self
-    }
-    pub fn path(mut self, path: ProjectPaths) -> Self {
-        let name = &self.project.name;
-        let domain = &self.project.domain.replace(".", "/");
-
-        let root = format!("{name}");
-        let main = format!("{root}/src/main");
-        let test = format!("{root}/src/test");
-        self.path = match path {
-            ProjectPaths::Root => root,
-            ProjectPaths::MainResources => format!("{main}/resources"),
-            ProjectPaths::TestResources => format!("{test}/resources"),
-            ProjectPaths::Working => format!("{main}/java/{domain}/{name}"),
-            ProjectPaths::TestWorking => format!("{test}/java/{domain}/{name}"),
-        };
-        self
-    }
-    pub fn template(mut self, template: Template) -> Self {
-        self.template = template;
-        self
-    }
-}
-
-fn craete_dirs(files: &Vec<ProjectFile>) -> Result<(), io::Error> {
-    for file in files {
-        fs::create_dir_all(&file.path)?;
     }
     Ok(())
 }
 
-fn create_files(files: &Vec<ProjectFile>) -> Result<(), io::Error> {
+fn create_files(files: &Vec<PathBuf>, project: &Project) -> Result<(), io::Error> {
     for file in files {
-        let new_file = File::create_new(format!("{}/{}", file.path, file.name));
-        if let Err(err) = new_file {
-            match err.kind() {
-                // path are defined in a way that
-                // there is no distinction between
-                // dirs and files so I discard them here.
-                io::ErrorKind::IsADirectory => (),
-                _ => return Err(err),
-            }
-        }
-        fs::write(
-            format!("{}/{}", file.path, file.name),
-            file.template.content.as_bytes(),
-        )?;
+        File::create_new(replace_placeholders(project, file))?;
     }
     Ok(())
 }
 
-fn get_structure(project: &Project) -> Vec<ProjectFile> {
-    vec![
-        ProjectFile::new(project)
-            .name("App.java")
-            .path(ProjectPaths::Working)
-            .template(Template::new("Main.java", project)),
-        ProjectFile::new(project)
-            .name("build.gradle.kts")
-            .path(ProjectPaths::Root)
-            .template(Template::new("gradle.kt", project)),
-        ProjectFile::new(project)
-            .name("AppTest.java")
-            .path(ProjectPaths::TestWorking)
-            .template(Template::new("Test.java", project)),
-        ProjectFile::new(project)
-            .name(".gitignore")
-            .path(ProjectPaths::Root)
-            .template(Template::new("gitignore", project)),
-        ProjectFile::new(project).path(ProjectPaths::MainResources),
-        ProjectFile::new(project).path(ProjectPaths::TestResources),
-    ]
+fn replace_placeholders(project: &Project, path: &Path) -> String {
+    let domain = project.domain.split_once(".").unwrap();
+    let path = path
+        .to_str()
+        .unwrap()
+        .replace("template", &project.name)
+        .replace("project-name", &project.name)
+        .replace("org", domain.0)
+        .replace("example", domain.1);
+    path
+}
+
+fn get_structure() -> Vec<PathBuf> {
+    let paths = fs::read_dir("template").unwrap();
+    paths
+        .into_iter()
+        .map(|path| get_childs(path.unwrap().path()))
+        .flatten()
+        .collect()
+}
+
+fn get_childs(path: PathBuf) -> Vec<PathBuf> {
+    let mut files = vec![];
+    if path.is_dir() {
+        for file in fs::read_dir(&path).unwrap() {
+            (&mut files).append(&mut get_childs(file.unwrap().path()))
+        }
+    } else {
+        files.push(path);
+    }
+
+    files.clone()
 }
